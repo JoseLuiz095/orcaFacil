1dd3c86a9ac50738d7ed21c0bda4703ce999ce89
José Luiz <82607322+JoseLuiz095@users.noreply.github.com>
2024-06-25 10:03:31 -0300

    Segunda video completo com Nabar e Footer completos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home.js";
+import Contact from "./components/pages/Contact.js";
+import Company from "./components/pages/Company.js";
+import NewProject from "./components/pages/NewProject.js";
+import Projects from "./components/pages/Projects"
+
+import Conteiner from './components/layout/Conteiner.js';
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
+
 function App() {
   return (
-    <p>OrçaFácil</p>
+    <Router>
+      <Navbar />
+      <Conteiner customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Conteiner>
+      <Footer />
+    </Router>
   );
 }
 
